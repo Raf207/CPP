11f5f2490feafe5380cbb17468490e152a75e26a
rafnasci <rafnasci@student.s19.be>
2025-02-05 23:12:43 +0100

    fonction search and add done for the contact

--- CPP Module 00/ex01/Contact.cpp
+++ CPP Module 00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: rafnasci <rafnasci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 21:49:27 by rafnasci          #+#    #+#             */
-/*   Updated: 2025/02/05 22:08:47 by rafnasci         ###   ########.fr       */
+/*   Updated: 2025/02/05 22:55:04 by rafnasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,37 @@ void	Contact::search(void)
 	std::cout << "Number :" << this->_number << std::endl;
 	std::cout << "Secret :" << this->_secret << std::endl;
 	return ;
+}
+
+void Contact::add(void)
+{
+	do{
+		std::cout << "Name : ";
+		std::getline(std::cin, this->_name);
+	} while (this->_name.empty());
+	do{
+		std::cout << "Surname : ";
+		std::getline(std::cin, this->_surname);
+	} while (this->_surname.empty());
+	do{
+		std::cout << "Nickname : ";
+		std::getline(std::cin, this->_nickname);
+	} while (this->_nickname.empty());
+	do{
+		std::cout << "Number : ";
+		std::getline(std::cin, this->_number);
+	} while (this->_number.empty()
+			|| this->_number.find_first_not_of("0123456789") != this->_number.npos);
+	do{
+		std::cout << "Secret : ";
+		std::getline(std::cin, this->_secret);
+	} while (this->_secret.empty());
+}
+
+int main()
+{
+	Contact instance;
+
+	instance.add();
+	return 0;
 }
--- CPP Module 00/ex01/Contact.hpp
+++ CPP Module 00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: rafnasci <rafnasci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 21:48:24 by rafnasci          #+#    #+#             */
-/*   Updated: 2025/02/05 22:09:24 by rafnasci         ###   ########.fr       */
+/*   Updated: 2025/02/05 22:26:47 by rafnasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define CONTACT_HPP
 
 #include <iostream>
+#include <string>
 
 class Contact
 {
