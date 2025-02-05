4ccfe32daea02c59d01476baa24c27564bcf1d0e
rafnasci <rafnasci@student.s19.be>
2025-02-05 23:16:33 +0100

    amelioration des fonciton search et add pour contact

--- CPP Module 00/ex01/Contact.cpp
+++ CPP Module 00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: rafnasci <rafnasci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 21:49:27 by rafnasci          #+#    #+#             */
-/*   Updated: 2025/02/05 22:55:04 by rafnasci         ###   ########.fr       */
+/*   Updated: 2025/02/05 23:15:30 by rafnasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,22 @@ Contact::~Contact()
 
 void	Contact::search(void)
 {
-	std::cout << "Name :" << this->_name << std::endl;
-	std::cout << "Surname :" << this->_surname << std::endl;
-	std::cout << "Nickname :" << this->_nickname << std::endl;
-	std::cout << "Number :" << this->_number << std::endl;
-	std::cout << "Secret :" << this->_secret << std::endl;
+	std::cout << "Name     : " << this->_name << std::endl;
+	std::cout << "Surname  : " << this->_surname << std::endl;
+	std::cout << "Nickname : " << this->_nickname << std::endl;
+	std::cout << "Number   : " << this->_number << std::endl;
+	std::cout << "Secret   : " << this->_secret << std::endl;
 	return ;
 }
 
 void Contact::add(void)
 {
 	do{
-		std::cout << "Name : ";
+		std::cout << "Name     : ";
 		std::getline(std::cin, this->_name);
 	} while (this->_name.empty());
 	do{
-		std::cout << "Surname : ";
+		std::cout << "Surname  : ";
 		std::getline(std::cin, this->_surname);
 	} while (this->_surname.empty());
 	do{
@@ -45,12 +45,12 @@ void Contact::add(void)
 		std::getline(std::cin, this->_nickname);
 	} while (this->_nickname.empty());
 	do{
-		std::cout << "Number : ";
+		std::cout << "Number   : ";
 		std::getline(std::cin, this->_number);
 	} while (this->_number.empty()
 			|| this->_number.find_first_not_of("0123456789") != this->_number.npos);
 	do{
-		std::cout << "Secret : ";
+		std::cout << "Secret   : ";
 		std::getline(std::cin, this->_secret);
 	} while (this->_secret.empty());
 }
@@ -60,5 +60,6 @@ int main()
 	Contact instance;
 
 	instance.add();
+	instance.search();
 	return 0;
 }
