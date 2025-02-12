ccaa34439406af17a79d6a8485891f373ae35910
Rafael <rafnasci@DESKTOP-GV0URCF>
2025-02-12 16:48:21 +0100

    end ex02

--- CPP Module 00/ex02/Account.cpp
+++ CPP Module 00/ex02/Account.cpp
@@ -6,17 +6,115 @@
 /*   By: rafnasci <rafnasci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 17:59:15 by rafnasci          #+#    #+#             */
-/*   Updated: 2025/02/06 18:11:50 by rafnasci         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:39:02 by rafnasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
-Account::Account(int initial_deposit) : _amount(initial_deposit), _accountIndex(Account::_nbAccounts), _nbDeposits(0), _nbWithdrawals(0)
+Account::Account(int initial_deposit) : _accountIndex(Account::_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
 {
-	
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
 	Account::_nbAccounts++;
+	Account::_totalAmount += initial_deposit;
 };
+
+Account::~Account( void )
+{
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
+	Account::_nbAccounts--;
+	Account::_totalAmount -= this->_amount;
+}
+
+void	Account::_displayTimestamp( void )
+{
+ 	std::time_t now = std::time(NULL);
+    std::tm* t = std::localtime(&now);
+
+    std::cout << "[" << t->tm_year + 1900
+              << (t->tm_mon < 9 ? "0" : "") << t->tm_mon + 1
+              << (t->tm_mday < 10 ? "0" : "") << t->tm_mday 
+              << "_"
+              << (t->tm_hour < 10 ? "0" : "") << t->tm_hour
+              << (t->tm_min < 10 ? "0" : "") << t->tm_min
+              << (t->tm_sec < 10 ? "0" : "") << t->tm_sec 
+			  << "] ";
+}
+
+int 	Account::getNbAccounts( void )
+{
+	return Account::_nbAccounts;
+}
+
+int		Account::getTotalAmount( void )
+{
+	return Account::_totalAmount;
+}
+
+int		Account::getNbDeposits( void )
+{
+	return Account::_totalNbDeposits;
+}
+
+int		Account::getNbWithdrawals( void )
+{
+	return Account::_totalNbWithdrawals;
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount()
+			<< ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	int	p_amount;
+
+	Account::_displayTimestamp();
+	p_amount = checkAmount();
+	this->_amount += deposit;
+	this->_nbDeposits++;
+	Account::_totalNbDeposits++;
+	Account::_totalAmount += deposit;
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << p_amount << ";deposit:" << deposit
+			<< ";amount:" << checkAmount() << ";nb_deposits:" << this->_nbDeposits << std::endl;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	int p_amount;
+
+	Account::_displayTimestamp();
+	p_amount = checkAmount();
+	if (this->_amount < withdrawal)
+	{
+		std::cout << "index:" << this->_accountIndex << ";p_amount:" << p_amount << ";withdrawal:refused" << std::endl;
+		return 0;
+	}
+	Account::_totalNbWithdrawals++;
+	Account::_totalAmount -= withdrawal;
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals++;
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << p_amount << ";withdrawal:" << withdrawal
+			<< ";amount:" << checkAmount() << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+	return 1;
+}
+
+void	Account::displayStatus( void ) const
+{
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << checkAmount() << ";deposits:"
+			<< this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+}
+
+int		Account::checkAmount( void ) const
+{
+	return this->_amount;
+}
 
 int		Account::_nbAccounts = 0;
 int		Account::_totalAmount = 0;
