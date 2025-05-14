967e4126df4f9db1c793ff7cc2bf1352cb632693
Rafael <rafnasci@DESKTOP-GV0URCF>
2025-05-15 00:07:41 +0200

    ex00 nouveau test

--- CPP Module 03/ex00/main.cpp
+++ CPP Module 03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rafnasci <rafnasci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 23:19:07 by rafnasci          #+#    #+#             */
-/*   Updated: 2025/05/14 23:36:53 by rafnasci         ###   ########.fr       */
+/*   Updated: 2025/05/15 00:06:50 by rafnasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,19 @@
 int main()
 {
 	ClapTrap igloo("igloo");
+	ClapTrap singe("singe");
 
-	for (int i = 0; i < 10; i++)
-		igloo.attack("jiji");
-	for (int i = 0; i < 10; i++)
-	{
-		igloo.takeDamage(1);
-		igloo.beRepaired(1);
-	}
+	// for (int i = 0; i < 10; i++)
+	// 	igloo.attack("jiji");
+	// for (int i = 0; i < 10; i++)
+	// {
+	// 	igloo.takeDamage(1);
+	// 	igloo.beRepaired(1);
+	// }
+	igloo.takeDamage(4);
+	singe.takeDamage(2);
+
+	singe = igloo;
+	singe.takeDamage(2);
+	igloo.takeDamage(4);
 }
